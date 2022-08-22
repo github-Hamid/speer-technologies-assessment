d0c84b9e46ba9e690b0d22f3bd59b330926e95fd
github-Hamid <h.rahimi.1953@gmail.com>
2022-08-21 23:02:44 -0400

    updating package

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
  app.use(requireHTTPS);
 app.use(express.static('./dist/speer-technologies-assessment'));
 app.get('/*', function(req, res) {
-  res.sendFile('index.html', {root: 'dist/speer-technologies-assessment/'}
+  res.sendFile('public/index.html', {root: 'dist/speer-technologies-assessment/'}
 );
 });
 
